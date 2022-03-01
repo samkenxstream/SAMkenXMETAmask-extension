445db1bd98207ec3a785c8875125e42631359843
Frederik Bolding <frederik.bolding@gmail.com>
2022-03-01 12:30:31 +0100

    Implementation based on RateLimitController

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -619,6 +619,26 @@ export default class MetamaskController extends EventEmitter {
       state: initState.SnapController,
       messenger: snapControllerMessenger,
     });
+
+    this.rateLimitController = new RateLimitController({
+      messenger: this.controllerMessenger.getRestricted({
+        name: 'RateLimitController',
+      }),
+      implementations: {
+        showNativeNotification: (origin, message) => {
+          const subjectMetadataState = this.controllerMessenger.call(
+            'SubjectMetadataController:getState',
+          );
+
+          const originMetadata = subjectMetadataState.subjectMetadata[origin];
+
+          this.platform._showNotification(
+            originMetadata?.name ?? origin,
+            message,
+          );
+        },
+      },
+    });
     ///: END:ONLY_INCLUDE_IN
 
     this.detectTokensController = new DetectTokensController({
@@ -1014,10 +1034,12 @@ export default class MetamaskController extends EventEmitter {
             type: MESSAGE_TYPE.SNAP_CONFIRM,
             requestData: confirmationData,
           }),
-        showNotification: this.controllerMessenger.call.bind(
-          this.controllerMessenger,
-          'NotificationControllerV2:show',
-        ),
+        showNotification: (origin, args) =>
+          this.controllerMessenger.call.bind(
+            this.controllerMessenger,
+            'RateLimitController:call',
+            { type: 'showNativeNotification', args: [origin, args[1]] },
+          ),
         updateSnapState: this.controllerMessenger.call.bind(
           this.controllerMessenger,
           'SnapController:updateSnapState',
